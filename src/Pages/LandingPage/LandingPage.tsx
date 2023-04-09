bcae7125a1cd1ae0e15e8ff0cc3c024da33c3c68
mattgoespro <mattyoung305@gmail.com>
2023-04-09 22:39:17 +0200

    Setup

---
+++ src/Pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,18 @@
+import "./LandingPage.css";
+
+function LandingPage() {
+  return (
+    <div className="m-auto flex max-w-lg flex-col">
+      <h1 className="text-left font-serif font-extralight italic">Addiction</h1>
+      <h2 className="mt-2 text-left font-sans text-lg font-light italic">
+        noun
+      </h2>
+      <h2 className="mt-2 text-left font-sans font-light">
+        a chronic, relapsing disorder characterized by compulsive drug seeking
+        and use despite adverse consequences
+      </h2>
+    </div>
+  );
+}
+
+export default LandingPage;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <RouterProvider router={App} />
+);
